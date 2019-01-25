7c6a093f3dcae68703c08e2c2bb44c4768aea1cf
kusnandartoni <kusnandartoni@gmail.com>
2019-01-25 17:14:41 +0700

    setup main ap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { AccountListComponent } from './pages/account-list/account-list.component';
+import { AccountDetailComponent } from './pages/account-detail/account-detail.component';
+import { AccountEditComponent } from './pages/account-edit/account-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'list', component: AccountListComponent},
+  {path: 'detail/:id', component: AccountDetailComponent},
+  {path: 'edit/:id', component: AccountEditComponent},
+  {path: '', redirectTo:'/list', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { AccountDetailComponent } from './pages/account-detail/account-detail.component';
+import { AccountListComponent } from './pages/account-list/account-list.component';
+import { AccountEditComponent } from './pages/account-edit/account-edit.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    AccountDetailComponent,
+    AccountListComponent,
+    AccountEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
