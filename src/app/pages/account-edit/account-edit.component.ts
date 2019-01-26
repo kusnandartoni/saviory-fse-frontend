b26c73f6a5cbef40f83bf0ea485359c614930807
kusnandartoni <kusnandartoni@gmail.com>
2019-01-26 17:07:13 +0700

    add input file button add upload function

--- src/app/pages/account-edit/account-edit.component.ts
+++ src/app/pages/account-edit/account-edit.component.ts
@@ -3,6 +3,9 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/models/user.model';
 import { UserService } from 'src/app/services/user.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { AngularFireUploadTask, AngularFireStorage } from 'angularfire2/storage';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-account-edit',
@@ -19,16 +22,22 @@ export class AccountEditComponent implements OnInit {
     phone: null,
     country: null,
     birthday: null,
-    isAdmin: false
+    isAdmin: false,
   }
   id = this.route.snapshot.params['id'];
   me: any = {};
+
+  task: AngularFireUploadTask;
+  percentage: Observable<number>;
+  snapshot: Observable<any>;
+  downloadUrl: Observable<string>;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private userService: UserService,
-    private authService: AuthService
+    private authService: AuthService,
+    private storage: AngularFireStorage
   ) {
     this.sessionCheck();
    }
@@ -65,4 +74,30 @@ export class AccountEditComponent implements OnInit {
   cancel(){
     this.router.navigateByUrl('/list');
   }
+
+  startUpload(event: FileList){
+    const file = event.item(0);
+    if(file.type.split('/')[0]!=='image'){
+      alert('unsupported file type');
+      return;
+    }
+    const path = `saviory/${new Date().getTime()}_${file.name}`;
+    const ref = this.storage.ref(path);
+    this.task = this.storage.upload(path, file);
+    this.percentage = this.task.percentageChanges();
+    this.snapshot = this.task.snapshotChanges();
+    this.task.snapshotChanges().pipe(
+      finalize(()=>{
+        this.downloadUrl = ref.getDownloadURL();
+        ref.getDownloadURL().subscribe(url=>{
+          console.log(url);
+        })
+      })
+    ).subscribe()
+  }
+
+  isActive(snapshot) {
+    return snapshot.state === 'running' && snapshot.bytesTransferred < snapshot.totalBytes;
+  }
+  
 }
